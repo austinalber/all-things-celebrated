252d3538195fc97c65d6eabc42b79f12fb681c65
Austin Alber <austinalber@austins-air.home>
2020-05-14 14:43:04 -0400

    redirection

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link, Redirect } from 'react-router-dom';
 // import Giphy from "../../components/Giphy"; 
 import "./style.css";
-import background from "../../resources/images/background.png"
+// import background from "../../resources/images/background.png"
 
 const About = props => {
   return (
@@ -15,20 +15,9 @@ const About = props => {
         <div className="bottom-div" style= {{ margin: '0 150px', lineHeight: '45px'}}>
           <h2 style={{ fontFamily: 'Gloria Hallelujah', marginBottom: '5%', marginTop: '5%'}}>Welcome!</h2>
           <p style={{ fontFamily: 'Gloria Hallelujah', fontSize: '20px'}}>
-            <span style={{ fontWeight: 800, letterSpacing: '5px' }}>Kor·gi</span> - the word originates from Wales, 
-            a mystical land of foggy rolling hills and castles.
-
-            In their Welsh language, it means little dog. 
-            A breed attributed for their warmth, cleverness, and curiosity. 
-
-            Like the dog, this application is based on those same <Link to="discover" style={{ textDecoration: 'blank', color: 'black'}}>quirks</Link>. 
-            We serve as a platform for little humans and the one inside all of us to express our own individuality
-            without the need of words, but with objects. So bring your dog or borrow one and lets explore together. 
-            Remember, it’s less of what we see but more of how we see things.
+            {/* Intro letter for store */}
           </p>
-          <Link to="/sign-up">
-            <button className="join-btn" type="button" style={{ fontSize: '25px'}}>Etsy</button>
-          </Link>
+            <button onClick={event => window.location.href="www.youtube.com"} className="join-btn" type="button" style={{ fontSize: '25px'}}>Etsy</button>
         </div>
     </div>
   );
